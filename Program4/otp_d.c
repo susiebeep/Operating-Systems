58000baf8821720ad751cd76f0a12b9fe3fd1ef5
Susan <hibberts@oregonstate.edu>
2020-06-01 11:31:08 -0700

    Server structure seems ok

--- Program4/otp_d.c
+++ Program4/otp_d.c
@@ -5,7 +5,6 @@
  ** Description:    SERVER
  ** *******************************************************************************/
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -19,6 +18,110 @@
 
 
 void error(const char *msg) { perror(msg); exit(1); } // Error function used for reporting issues
+
+
+/* ********************************************************************************** 
+ ** Description:
+ ** Input(s): 	File descriptor number representing new client connection
+ ** Output(s): 	 
+ ** Returns: 
+ ** *******************************************************************************/
+
+void childCon(int newConnFD)
+{
+	// Child process sleeps for 2 seconds
+	sleep(2);	
+
+	// Read the client's message from the socket
+	char buffer[256];
+	int charsRead;
+	memset(buffer, '\0', 256);
+	charsRead = recv(newConnFD, buffer, 255, 0);
+	if (charsRead < 0) error("ERROR reading from socket");
+
+	// Extract mode, user and encrypted message sent by the client
+	char mode[16];
+	char user[32];
+	char encryptedMsg[1024];
+	memset(encryptedMsg, '\0', sizeof(encryptedMsg));
+	char* token;
+
+	token = strtok(buffer, "-");
+	int field = 0;
+
+	while (token != NULL)
+	{
+		if (field == 0)
+		{
+			strcpy(user, token);
+		}
+		else if (field == 1)
+		{
+			strcpy(mode, token);
+		}
+		else if (field == 2)
+		{
+			strcpy(encryptedMsg, token);
+		}
+		field++;
+		token = strtok(NULL, "-");		
+	}
+	// *******************************************************************************************
+	// POST MODE
+	if (strcmp(mode, "post") == 0)
+	{
+		// add a newline char to end of encrypted message, so the encrypted
+		// text file ends with a newline character as per the specifications
+		int msgLength = strlen(encryptedMsg);
+		encryptedMsg[msgLength] = '\n';
+		
+		// create and open a directory for the user
+		mkdir(user, 0755);
+		DIR* userDir = opendir(user);
+
+		// file name to store encrypted message
+		char cipherText[64];
+		char* fileName = "cipherText";
+
+		// generate a random number between 0 and 100 and append to the end of the file name
+		int randNo = rand() % 100;
+		sprintf(cipherText, "%s%d", fileName, randNo);
+
+		// create a filepath variable and store the path to the user's directory
+		char* filepath = malloc(128*sizeof(char));
+		sprintf(filepath, "./%s/%s", user, cipherText);
+		int filedescriptor = open(filepath, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	
+		// write the encrypted message to a file
+		write(filedescriptor, encryptedMsg, strlen(encryptedMsg)*sizeof(char));		
+
+		// print the path to the encrypted file	
+		printf("\n%s\n", filepath);
+		fflush(stdout);
+			
+		// reset the file pointer back to the beginning of the file
+		lseek(filedescriptor, 0, SEEK_SET);
+
+		// close the user directory
+		closedir(userDir);
+	}	
+	// *******************************************************************************************
+	// GET MODE
+	else if (strcmp(mode, "get") == 0)
+	{
+	// retrieve the contents of the oldest file for the user and send them to otp
+	// then delete the ciphertext file
+	}			
+
+}
+
+
+/* ********************************************************************************** 
+ ** Description: Main function
+ ** Input(s): 	
+ ** Output(s): 	 
+ ** Returns: 
+ ** *******************************************************************************/
 
 int main(int argc, char *argv[])
 {
@@ -46,15 +149,16 @@ int main(int argc, char *argv[])
 	if (bind(listenSocketFD, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) // Connect socket to port
 		error("ERROR on binding");
 	listen(listenSocketFD, 5); // Flip the socket on - it can now receive up to 5 connections
+	sizeOfClientInfo = sizeof(clientAddress); // Get the size of the address for the client that will connect
+
 
 	// Accept a connection, blocking if one is not available until one connects
 	while(1)
-	{
-		sizeOfClientInfo = sizeof(clientAddress); // Get the size of the address for the client that will connect
+	{	
 		establishedConnectionFD = accept(listenSocketFD, (struct sockaddr *)&clientAddress, &sizeOfClientInfo); // Accept
 		if (establishedConnectionFD < 0) error("ERROR on accept");
 
-		// create a child process
+		// create a new child process
 		pid_t spawnPid = -5;
 		spawnPid = fork();
 	
@@ -68,94 +172,13 @@ int main(int argc, char *argv[])
 			// Child process closes the listening socket
 			close(listenSocketFD); 
 
-			// Child process sleeps for 2 seconds
-			sleep(2);
+			// pass file descriptor of new connection as argument
+			childCon(establishedConnectionFD);
 
-			// Read the client's message from the socket
-			memset(buffer, '\0', 256);
-			charsRead = recv(establishedConnectionFD, buffer, 255, 0);
-			if (charsRead < 0) error("ERROR reading from socket");
-
-			// Extract mode, user and encrypted message sent by the client
-			char mode[16];
-			char user[32];
-			char encryptedMsg[1024];
-			memset(encryptedMsg, '\0', sizeof(encryptedMsg));
-			char* token;
-
-			token = strtok(buffer, "-");
-			int field = 0;
-
-			while (token != NULL)
-			{
-				if (field == 0)
-				{
-					strcpy(user, token);
-				}
-				else if (field == 1)
-				{
-					strcpy(mode, token);
-				}
-				else if (field == 2)
-				{
-					strcpy(encryptedMsg, token);
-				}
-				field++;
-				token = strtok(NULL, "-");		
-			}
-			// *******************************************************************************************
-			// POST MODE
-			if (strcmp(mode, "post") == 0)
-			{
-				// add a newline char to end of encrypted message, so the encrypted
-				// text file ends with a newline character as per the specifications
-				int msgLength = strlen(encryptedMsg);
-				encryptedMsg[msgLength] = '\n';
-				
-				// create and open a directory for the user
-				mkdir(user, 0755);
-				DIR* userDir = opendir(user);
-		
-				// file name to store encrypted message
-				char cipherText[64];
-				char* fileName = "cipherText";
-
-				// generate a random number between 0 and 100 and append to the end of the file name
-				int randNo = rand() % 100;
-				sprintf(cipherText, "%s%d", fileName, randNo);
-
-				// create a filepath variable and store the path to the user's directory
-				char* filepath = malloc(128*sizeof(char));
-				sprintf(filepath, "./%s/%s", user, cipherText);
-				int filedescriptor = open(filepath, O_RDWR | O_CREAT | O_TRUNC, 0600);
-			
-				// write the encrypted message to a file
-				write(filedescriptor, encryptedMsg, strlen(encryptedMsg)*sizeof(char));		
+			// exit oncechild process returns and re-enter outer infinite while loop
+			exit(0);	
 	
-				// print the path to the encrypted file	
-				printf("%s\n", filepath);
-				fflush(stdout);
-					
-				// reset the file pointer back to the beginning of the file
-				lseek(filedescriptor, 0, SEEK_SET);
-
-				// close the user directory
-				closedir(userDir);
-
-				// exit from the child process
-				exit(0);
-			}	
-	
-			// *******************************************************************************************
-			// GET MODE
-			else if (strcmp(mode, "get") == 0)
-			{
-			// retrieve the contents of the oldest file for the user and send them to otp
-			// then delete the ciphertext file
-				exit(0);
-			}
-		}	
-			
+		}		
 		// if spawnPid is not equal to 0	
 		else
 		{
@@ -163,6 +186,7 @@ int main(int argc, char *argv[])
 		}	
 
 	}
-	
+
+	close(listenSocketFD);
 	return 0;
 }
